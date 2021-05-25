798b049726ca842212910c2961af9a4ab1ae1c52
David Ikin <shadow2272@aol.com>
2021-05-25 02:38:37 +0100

    Content upload and code enhancements.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+/* alert('i work if you can see this'); */
+
 const toggleBtn = document.getElementById('toggleBtn');
+const body = document.getElementById('body');
+
+toggleBtn.addEventListener('click', () =>
+  body.classList.toggle('darkTheme')
+);
